07b8446e7e499b08e335e3038b02632da9596e45
Yiming Wei <weiyiming.seu@gmail.com>
2022-07-18 23:17:41 +0800

    动态规划-5.最长回文子串

---
+++ LongestPalindrome.py
@@ -0,0 +1,29 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        if n < 2:
+            return s
+        max_len = 1
+        begin = 0
+        dp = [[False]*n for _ in range(n)]
+        # 当i=j, s[i]=s[j]
+        for i in range(n):
+            dp[i][i] = True
+        for L in range(2, n+1):
+            for i in range(0, n):
+                # 字串长度L=j-i+1
+                j = L+i-1
+                if j >= n:
+                    break
+                if s[i] != s[j]:
+                    dp[i][j] = False
+                else:
+                    # 即L为2或3时
+                    if j-i < 3:
+                        dp[i][j] = True
+                    else:
+                        dp[i][j] = dp[i+1][j-1]
+                if dp[i][j] and j-i+1 > max_len:
+                    max_len = j-i+1
+                    begin = i
+        return s[begin:begin+max_len]
