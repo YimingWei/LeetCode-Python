3fe1ef5c9d8c28b29209d04c12581b2a36a1c138
Yiming Wei <weiyiming.seu@gmail.com>
2022-08-30 22:39:02 +0800

    递归-105.从前序与中序遍历序列构造二叉树

---
+++ BuildTree.py
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        while inorder:
+            idx = inorder.index(preorder.pop(0))
+            root = TreeNode(inorder[idx])
+            root.left = self.buildTree(preorder, inorder[:idx])
+            root.right = self.buildTree(preorder, inorder[idx+1:])
+            return root
