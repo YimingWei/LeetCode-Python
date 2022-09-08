b051a5490c57718f0af36fab152935cdeca71c8d
Yiming Wei <weiyiming.seu@gmail.com>
2022-09-08 22:25:15 +0800

    动态规划-152.乘积最大子数组

---
+++ MaxProduct.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        res = nums[0]
+        pre_max = nums[0]
+        pre_min = nums[0]
+        for num in nums[1:]:
+            cur_max = max(pre_max * num, pre_min * num, num)
+            cur_min = min(pre_max * num, pre_min * num, num)
+            res = max(res, cur_max)
+            pre_max = cur_max
+            pre_min = cur_min
+        return res
