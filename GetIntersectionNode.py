8b8862a1f489b2d354c924d64d98821d9c6bcdef
Yiming Wei <weiyiming.seu@gmail.com>
2022-09-13 21:59:37 +0800

    160.相交链表

---
+++ GetIntersectionNode.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        A = headA
+        B = headB
+        while A != B:
+            A = A.next if A else headB
+            B = B.next if B else headA
+        return A
