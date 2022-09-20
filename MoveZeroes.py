55bd6eb0dab0da09a21958dd782f2378cbba673a
Yiming Wei <weiyiming.seu@gmail.com>
2022-09-20 17:06:36 +0800

    283.移动零

---
+++ MoveZeroes.py
@@ -0,0 +1,12 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = 0
+        for num in nums:
+            if num != 0:
+                nums[i] = num
+                i += 1
+        for j in range(i, len(nums)):
+            nums[j] = 0
